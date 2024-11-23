65ac6eb71d44ae8a747f4a4fa44bd0f09cbb4f7a
Amaury Perea Matsumura <amaury.matsumura@gmail.com>
2024-11-22 21:44:11 -0600

    Ejercicio finalizado

--- app/src/main/java/com/amaurypm/idiplodm/MainActivity.kt
+++ app/src/main/java/com/amaurypm/idiplodm/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.amaurypm.idiplodm
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.amaurypm.idiplodm.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var user: FirebaseUser? = null
+    private var userId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +32,36 @@ class MainActivity : AppCompatActivity() {
         }
 
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        user = firebaseAuth.currentUser
+        userId = user?.uid
+
+        binding.tvUsuario.text = user?.email
+
+        //Revisamos si el correo no está verificado
+
+        if(user?.isEmailVerified != true){
+            //No ha verificado su correo
+            binding.tvCorreoNoVerificado.visibility = View.VISIBLE
+            binding.btnReenviarVerificacion.visibility = View.VISIBLE
+
+            binding.btnReenviarVerificacion.setOnClickListener {
+                user?.sendEmailVerification()?.addOnSuccessListener {
+                    message("El correo de verificación ha sido reenviado")
+                }?.addOnFailureListener {
+                    message("Error, no se pudo reenviar el correo")
+                }
+            }
+        }
+
+        binding.btnCerrarSesion.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+            message("Sesión cerrada exitosamente")
+            finish()
+        }
+
     }
 }
